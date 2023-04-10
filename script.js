fe6f1e3b9d888063fbf06b403de4a2e5d3ead8bc
Rahul Sharma <rahulsh3105@gmail.com>
2023-04-10 17:00:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,29 +2,39 @@ class Animal {
   constructor(species) {
     this._species = species;
   }
-
   get species() {
     return this._species;
   }
-
   makeSound() {
-    console.log('The ' + this.species + ' makes a sound');
+    console.log("The " + this.species + " makes a sound");
   }
 }
-
 class Cat extends Animal {
   purr() {
-    console.log('The ' + this.species + ' makes a sound');
+    console.log("purr");
   }
 }
-
 class Dog extends Animal {
   bark() {
-    console.log('The ' + this.species + ' makes a sound');
+    console.log("woof");
   }
 }
-
 // Do not change the code below this line
 window.Animal = Animal;
-window.Cat = Cat;
 window.Dog = Dog;
+window.Cat = Cat;
+
+function getElementsByAttribute(attribute, value) {
+  const elementsWithAttribute = [];
+  const allElements = document.getElementsByTagName("*");
+
+  for (let i = 0; i < allElements.length; i++) {
+    if (allElements[i].getAttribute(attribute) === value) {
+      elementsWithAttribute.push(allElements[i]);
+    }
+  }
+
+  return elementsWithAttribute;
+}
+ const elementsWithBarBaz = getElementsByAttribute("data-bar", "baz");
+    console.log(elementsWithBarBaz);
