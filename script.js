eae9c326fd9a4aeae86c919e9d5f3516b605675d
Rahul Sharma <rahulsh3105@gmail.com>
2023-04-10 16:54:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
- class Animal {
-    constructor(species) {
-      this._species = species;
-    }
-    
-    get species() {
-      return this._species;
-    }
-    
-    makeSound() {
-      console.log("The " + this._species + " makes a sound.");
-    }
+class Animal {
+  constructor(species) {
+    this._species = species;
   }
 
-  // Complete the Cat class
-  class Cat extends Animal {
-    purr() {
-      console.log("The " + this._species + " purrs.");
-    }
+  get species() {
+    return this._species;
   }
 
-  // Complete the Dog class
-  class Dog extends Animal {
-    bark() {
-      console.log("The " + this._species + " barks.");
-    }
+  makeSound() {
+    console.log('The ' + this.species + ' makes a sound');
   }
+}
 
-  // Do not change the code below this line
-  window.Animal = Animal;
-  window.Dog = Dog;
-  window.Cat = Cat;
+class Cat extends Animal {
+  purr() {
+    console.log('The ' + this.species + ' purrs.');
+  }
+}
+
+class Dog extends Animal {
+  bark() {
+    console.log('The ' + this.species + ' barks.');
+  }
+}
+
+// Do not change the code below this line
+window.Animal = Animal;
+window.Cat = Cat;
+window.Dog = Dog;
